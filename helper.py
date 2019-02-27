aab52f89310f407489641134b3c887716f81b13a
HouManIp <benip1840@berkeley.edu>
2019-02-27 13:08:19 -0800

    beta 1.03

--- helper.py
+++ helper.py
@@ -10,11 +10,11 @@ class HelperFucClass:
 
 	profkey = 'inIyO1begWSRqsYtxS7m6p09PSyq7Qiw7fxzV2qN'
 	mykey = 'JPk6gFJ2IAI7YNFQuXQ7wIwUyPXTMxoKAriLzZU2'
-	key = mykey
+	api_key = profkey
 	nd_url = 'https://api.nal.usda.gov/ndb/V2/reports?format=json&type=b'
 	mode = 'ui'
-
-
+	_ora = []
+	_exact_word = 0
 	def _helper1(x):
 		return x.replace("'", "").replace('"', "").replace(",", "").replace("&", "").replace(" ", "").replace("-","").lower()
 
@@ -55,75 +55,65 @@ class HelperFucClass:
 			pd.Series(x).groupby(a).agg(lambda y : sum(y)/len(y))
 			return  """
 
+		if len(foods) == 0:
 
-		if HelperFucClass.mode == 'ui':
+			return []
 
-			if len(foods) == 0:
-
-				return []
-
-
-			data1 = classificationforfoods.search_user(foods[0],c)
-
-			data1 = requests.get(HelperFucClass.nd_url , params = tuple(( ('ndbno',i['ndbno']) for i in data1)) + (('api_key', HelperFucClass.mykey),)    ).json()
-
-			return data_recursive_helper(data1['foods']) + HelperFucClass.ndhelper1(foods[1:],c)
-
-		if HelperFucClass.mode == 'test':
-			
-
-			data1 = classificationforfoods.search_insider(foods[0])
-
-			data1 = requests.get(HelperFucClass.nd_url , params = tuple(( ('ndbno',i['ndbno']) for i in data1)) + (('api_key', HelperFucClass.mykey),)    ).json()
-
-			data1 = data_recursive_helper(data1['foods'])
-
-			return pd.DataFrame(data1,columns = ['Food Name','Nutrients', 'Value']).set_index(['Food Name' ,'Nutrients' ])
+		try:
+			data1 = classificationforfoods.search_user(foods[0],c) if HelperFucClass.mode == 'ui' else classificationforfoods.search_insider(foods[0]) if HelperFucClass.mode == 'test' else None
+			data1 = requests.get(HelperFucClass.nd_url , params = tuple(( ('ndbno',i['ndbno']) for i in data1)) + (('api_key', HelperFucClass.api_key),)    ).json()
+		
+		except:
+			HelperFucClass._ora += [foods[0]]
+			return HelperFucClass.ndhelper1(foods[1:],c)
+		 
 
 
-
+		return data_recursive_helper(data1['foods']) + HelperFucClass.ndhelper1(foods[1:],c)
 
 
 
 class classificationforfoods:
 
 	def search_user(food, c = 'safeway',maxx = 1500, url = 'https://api.nal.usda.gov/ndb/search?format=json'):
+		qwertyui = "UPC [0-9]+"
+		punct_re = r'[^\w ]|  +'
+		food = re.sub(punct_re," ",food.lower())
+		fil_re =  '|'.join(food.split()) if len(food.split()) > 1 else food.lower()
+		print(fil_re)
+		data = requests.get(url , params = (('q', food),('api_key', HelperFucClass.api_key),('max',maxx)))
 
-		data = requests.get(url , params = (('q', food),('api_key', HelperFucClass.profkey),('max',maxx)))
+		tempx =  np.array([i for i in data.json()['list']['item'] if re.findall(fil_re, re.sub(punct_re ," ",i['name'].lower()) ) ]) if HelperFucClass._exact_word  else np.array([i for i in data.json()['list']['item'] if len( re.sub(punct_re ," ",i['name'].lower())) == len(fil_re.split('|')) ])
 
-		tempx =  np.array([i for i in data.json()['list']['item']])
-
-
-		if len(tempx) != 1:
+		#tempx =  np.array([i for i in data.json()['list']['item']])
 
 
-			#temp2 = [   (' '.join(i['name'].split()[:[k for j,k in zip(i['name'].split(), range(len(i['name'].split()))) if 'UPC' in j][0]])) for i in tempx]
-			pprint.pprint([ str(j) + ': '+i['name'] for i,j in zip(tempx,range(len(tempx)))])
-			print ('========================================================================')
-			print ('please input the products you want following the next line ')
-			print ('For instance:')
-			#print ('1. x1')
-			#print ('2. x1/x2/x3')
-			print ('3. index(0,1,2,3)')
-			print ('4. esc')
-			_input = input('>>>>> ')
 
-			if 'index' in _input:
-				temp3 = [int(i) for i in _input.replace('index',"").replace('(',"").replace(')','').split(',')]
+		#temp2 = [   (' '.join(i['name'].split()[:[k for j,k in zip(i['name'].split(), range(len(i['name'].split()))) if 'UPC' in j][0]])) for i in tempx]
+		pprint.pprint([ str(j) + ': '+i['name'] for i,j in zip(tempx,range(len(tempx)))][:20])
+		print ('========================================================================')
+		print ('please input the products you want following the next line ')
+		print ('For instance:')
+		#print ('1. x1')
+		#print ('2. x1/x2/x3')
+		print ('3. index(0,1,2,3)')
+		print ('4. esc')
+		_input = input('>>>>> ')
+		if 'index' in _input:
+			temp3 = [int(i) for i in _input.replace('index',"").replace('(',"").replace(')','').split(',')]
 	
-				return [i for i,j in zip(tempx,range(len(tempx))) if j in temp3 ]
+			return [i for i,j in zip(tempx,range(len(tempx))) if j in temp3 ]
 
 
-			elif '/' in _input:
-				temp3 = np.array([ HelperFucClass._helper1(i) for i in  _input.replace("'", "").replace('"', "").split("/")])
+		elif '/' in _input:
+			temp3 = np.array([ HelperFucClass._helper1(i) for i in  _input.replace("'", "").replace('"', "").split("/")])
 	
 
-				return tempx[[True if HelperFucClass._helper1(i) in temp3 else False for i in temp2]]
-			elif 'esc' == _input:
-				return None
-			else:
-				return [ i for i in tempx if _input.lower() in i['name'].lower()][0]
-
+			return tempx[[True if HelperFucClass._helper1(i) in temp3 else False for i in temp2]]
+		elif 'esc' == _input:
+			return None
+		else:
+			return [ i for i in tempx if _input.lower() in i['name'].lower()][0]
 		return list(tempx)
 
 
@@ -133,35 +123,36 @@ class classificationforfoods:
 
 		punct_re = r'[^\w ]'
 		temp_raw = 'raw ' + re.sub(punct_re," ",food)
-		abcd = re.sub(punct_re," ",'Broccoli raab, raw'.lower())
-		print (len(abcd.split()))
+		#abcd = re.sub(punct_re," ",'Broccoli raab, raw'.lower())
+		#print (len(abcd.split()))
 
 		if not cache_ndb._boolean_exist('ndbno','raw ' + food, index = True):
 
 			
 
-			data2 = requests.get(url , params = (('q', temp_raw),('api_key', HelperFucClass.mykey),('max',maxx), ('ds','Standard Reference'))).json()['list']['item']
-			data1 = requests.get(url , params = (('q', temp_raw),('api_key', HelperFucClass.mykey),('max',maxx))).json()['list']['item']
+			data2 = requests.get(url , params = (('q', temp_raw),('api_key', HelperFucClass.api_key),('max',maxx), ('ds','Standard Reference'))).json()['list']['item']
+			data1 = requests.get(url , params = (('q', temp_raw),('api_key', HelperFucClass.api_key),('max',maxx))).json()['list']['item']
 
 			cache_ndb._update('ndbno', {temp_raw.lower(): data1}, index = 0)
 			cache_ndb._update('ndbno', {temp_raw.lower(): data2}, index = 1)
 
-			print (1)
+
 
 		else:
 
 			data1 = cache_ndb._caches['ndbno'][0][temp_raw.lower()]
 			data2 = cache_ndb._caches['ndbno'][1][temp_raw.lower()]
-			print (222)
+	
 
-		temp_list = [i if len(re.findall( 'raw|'+food.lower(),re.sub(punct_re," ",i['name'].lower()) )) == len(temp_raw.split()) and len(re.sub(punct_re," ",i['name'].lower()).split()) == len(temp_raw.split())  else None if not len(re.sub(punct_re," ",i['name'].lower()).split()) == len(temp_raw.split()) + 2 or not 'UPC' in i['name'] else i for i in data1]
-		temp_list = [i for i in temp_list if i != None] 
+		#temp_list = [i if len(re.findall( 'raw|'+food.lower(),re.sub(punct_re," ",i['name'].lower()) )) == len(temp_raw.split()) and len(re.sub(punct_re," ",i['name'].lower()).split()) == len(temp_raw.split())  else None if not len(re.sub(punct_re," ",i['name'].lower()).split()) == len(temp_raw.split()) + 2 or not 'UPC' in i['name'] else i for i in data1]
+		#temp_list = [i for i in temp_list if i != None] 
+		temp_list = None
 		temp_list2 =  [i for i in data2 if len(re.findall( 'raw|'+food.lower(),re.sub(punct_re," ",i['name'].lower()) )) == len(temp_raw.split()) and len(re.sub(punct_re," ",i['name'].lower()).split()) == len(temp_raw.split())]
 
+		
 
 		if temp_list or temp_list2:
-			print (temp_list2)
-			print (33333333)
+
 			return temp_list2 if temp_list2 else temp_list
 
 
--- nd_m.py
+++ nd_m.py
@@ -21,8 +21,7 @@ from helper import HelperFucClass as hfc
 
 
 
-def nutrition_dataframe_beta1(foods = ['milk','broccoli'],c = 'safeway',key = 'JPk6gFJ2IAI7YNFQuXQ7wIwUyPXTMxoKAriLzZU2', debug = 0, mode = 'ui'):
-	hfc.mode = mode
+def nutrition_dataframe_beta1(foods = ['milk','broccoli'],c = 'safeway',exact_word = True, key = 'JPk6gFJ2IAI7YNFQuXQ7wIwUyPXTMxoKAriLzZU2', debug = 0, mode = 'ui'):
 
 	if debug == 1:
 		d1 = ['45109162', '45361064']
@@ -30,21 +29,21 @@ def nutrition_dataframe_beta1(foods = ['milk','broccoli'],c = 'safeway',key = 'J
 		df = pd.DataFrame(hfc.debug1(d2,key = key))
 		return df
 
-
-	if mode == 'ui':
-
-	
-		df = pd.DataFrame(hfc.ndhelper1( foods, c),columns = ['Food Name','Nutrients', 'Value']).set_index(['Food Name' ,'Nutrients' ])
-		return df
-
-
-	if mode == 'test':
-		return hfc.ndhelper1(foods,c)
-
-	if mode == 'test2':
-		return hfc.ndhelper1(foods,c)
+	hfc.mode = mode
+	hfc._exact_word = exact_word
+	df = pd.DataFrame(hfc.ndhelper1( foods, c),columns = ['Food Name','Nutrients', 'Value']).set_index(['Food Name' ,'Nutrients' ])
+	print ( "["+", ".join(hfc._ora) + ']  do/does not exist'  if len(hfc._ora) != 0 else '')
+	hfc._ora = []
+	return df
+	"""except:
+		print (hfc._ora)
+		print ( " ".join(hfc._ora) + 'do/does not exist'  if len(hfc._ora) != 0 else '')
+		return"""
 
 
+def test():
+	return pd.read_csv('Project2Data_Sheet1.csv')
+	#return requests.get('https://docs.google.com/spreadsheets/u/1/d/1d8t2PHjkGs2pOcx1aTfWZBCiJtFV-QCScxws_B98-aw/edit#gid=0')
 def merge_nd():
 	x = 'https://docs.google.com/spreadsheets/u/1/d/1d8t2PHjkGs2pOcx1aTfWZBCiJtFV-QCScxws_B98-aw/edit#gid=0'
 	pass
@@ -56,5 +55,8 @@ def open_nd():
 	pass
 
 
-x = nutrition_dataframe_beta1( foods = ['frozen broccoli'])
-
+#x = nutrition_dataframe_beta1( foods = ['frozen broccoli'])
+y = test()
+yy = y['Food'].iloc
+ora = nutrition_dataframe_beta1
+asdf = ora(foods= list(yy[5:7]),mode='test')
